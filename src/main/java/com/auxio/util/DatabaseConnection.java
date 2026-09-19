27462fce68ff9924dfc9864772d948213c33d60d
nishanth-1431 <hinishanth777@gmail.com>
2026-09-19 16:28:56 +0530

    feat: add JDBC database connection utility

--- src/main/java/com/auxio/util/DatabaseConnection.java
+++ src/main/java/com/auxio/util/DatabaseConnection.java
@@ -1,5 +1,19 @@
 package com.auxio.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DatabaseConnection {
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USERNAME = dotenv.get("DB_USERNAME");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() throws Exception {
+    return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+}
 
 }
